72060ba72f99146e7206122696742075982c8028
masontuft <69863254+masontuft@users.noreply.github.com>
2022-04-24 15:31:52 -0600

    changePassword.js implementation

--- shop_tools/changePassword_script.js
+++ shop_tools/changePassword_script.js
@@ -1,12 +1,49 @@
-var token = new URLSearchParams( window.location.search );
-token = token.get('token')
+// var token = new URLSearchParams( window.location.search );
+// token = token.get('token')
+
+var token = sessionStorage.getItem('token')
 console.log(token)
 
 const changePassword = document.getElementById("changePassword");
 
 changePassword.addEventListener("submit", (e) => {
     e.preventDefault();
+    var current_password = document.getElementById("currentPassword").value;
+    var new_password = document.getElementById("newPassword").value;
+    var re_enter_password = document.getElementById("re-enterPassword").value;
     
+    if (new_password == re_enter_password) {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer 97f94bee48b8ebf793f0c445c1ade27070625622");
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("current_password", current_password);
+        urlencoded.append("new_password", new_password);
+        urlencoded.append("token", token);
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+        }
+        
+
+        fetch("https://jmrcycling.com:3001/changeShopPassword", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result)
+            window.alert("Password changed successfully")
+        })
+        .catch(error => console.log('error', error));
+    }
+    else if (new_password != re_enter_password) {
+        window.alert("Your new password and re-entered passwords don't match");
+    }
+    else {
+        window.alert("Something went wrong, please try again");
+    }
 })
 
 function myFunction() {
